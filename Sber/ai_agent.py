a5a7a0a4349a554bf94aae3acf70793811f15a41
SWEET_LUCAS <ansalekseeva1@gmail.com>
2025-07-29 12:53:11 +0300

    fix uid

--- Sber/ai_agent.py
+++ Sber/ai_agent.py
@@ -3,8 +3,8 @@ import logging
 from dotenv import load_dotenv
 from dialog_bot_sdk.bot import DialogBot
 from dialog_bot_sdk.entities.messaging import UpdateMessage
-from dialog_bot_sdk.models import InteractiveMedia, InteractiveButton
 from dialog_bot_sdk.entities.messaging import MessageContentType, MessageHandler, CommandHandler
+from dialog_bot_sdk.entities.users import User
 
 from ai_agent import check_idea_with_gigachat_local, generate_files
 
@@ -16,6 +16,8 @@ os.environ["GRPC_DEFAULT_SSL_ROOTS_FILE_PATH"] = '/home/sigma.sbrf.ru@22754707/�
 
 BOT_TOKEN = os.getenv("DIALOG_BOT_TOKEN")
 
+logging.basicConfig(level=logging.INFO)
+
 TEMPLATE_FIELDS = [
     "Название", "Что хотим улучшить?", "Какие данные поступают агенту на выход?",
     "Как процесс выглядит сейчас? as-is", "Какой результат нужен от агента?",
@@ -24,14 +26,33 @@ TEMPLATE_FIELDS = [
 
 user_states = {}
 
-
-def text_handler(message: UpdateMessage) -> None:
-    user_id = message.sender.uid
-    msg = message.message.text_message.text.strip()
-    peer = message.peer
+def text_handler(update: UpdateMessage) -> None:
+    message = update.message
+    user_id = message.sender_uid
+    msg = message.text_message.text.strip()
+    peer = update.peer
 
     state = user_states.get(user_id, {})
 
+    logging.info(f"📩 Получено сообщение: {msg} от пользователя {user_id}")
+
+    if msg.lower() in ["/start", "./start", "start"]:
+        start_handler(update)
+        return
+    elif msg.lower() in ["/idea", "idea","идея","придумал"]:
+        idea_handler(update)
+        return
+    elif msg.lower() in ["/ai", "ai","агент","агентолог"]:
+        agent_handler(update)
+        return
+    elif msg.lower() in ["/help","help","помощь"]:
+        help_handler(update)
+        return
+    elif msg.lower() in ["/Кто поможет?", "ai_agent","агенты","агентолог"]:
+        group_handler(update)
+        return
+
+    # Обработка идеи в свободной форме
     if state.get("mode") == "freeform":
         user_data = {"Описание в свободной форме": msg}
         bot.messaging.send_message(peer, "🔍 Отправляю идею в GigaChat...")
@@ -46,6 +67,7 @@ def text_handler(message: UpdateMessage) -> None:
         user_states.pop(user_id)
         return
 
+    # Обработка идеи по шаблону
     elif state.get("mode") == "template":
         step = state.get("step", 0)
         state.setdefault("data", {})
@@ -67,24 +89,7 @@ def text_handler(message: UpdateMessage) -> None:
             user_states.pop(user_id)
         return
 
-    if msg == "У меня есть идея!💌":
-        user_states[user_id] = {
-            "mode": "choose",
-            "step": None,
-            "data": {},
-        }
-        bot.messaging.send_message(
-            peer,
-            "📝 Как хотите описать идею?",
-            [InteractiveMedia(
-                actions=[
-                    InteractiveButton("Давай шаблон!"),
-                    InteractiveButton("Я могу и сам написать"),
-                ]
-            )]
-        )
-        return
-
+    # Инициация шаблона
     if msg == "Давай шаблон!":
         user_states[user_id] = {
             "mode": "template",
@@ -94,39 +99,47 @@ def text_handler(message: UpdateMessage) -> None:
         bot.messaging.send_message(peer, f"1️⃣ {TEMPLATE_FIELDS[0]}:")
         return
 
+    # Инициация свободной формы
     if msg == "Я могу и сам написать":
         user_states[user_id] = {"mode": "freeform"}
         bot.messaging.send_message(peer, "✍️ Введите вашу идею в свободной форме:")
         return
 
-    bot.messaging.send_message(
-        peer,
-        "👋 Привет, @lucas_no_way! \n"
-        "Меня зовут Агентолог, я помогу тебе с идеями для AI-агентов.\n\n"
-        "Вот что я могу сделать:\n"
-        "1. У меня есть идея!💡\n"
-        "   Я помогу тебе узнать, насколько твоя идея уникальна!\n\n"
-        "2. АИ-агенты?📍\n"
-        "   АИ-агенты разрабатываются каждый день, здесь мы собрали самый свежий список агентов!\n\n"
-        "3. Кто поможет?💬\n"
-        "   Агентов очень много и не всегда можно найти, кто их разрабатывает. Давай подскажем, кто эти люди!\n\n"
-        "4. Поддержка📝\n"
-        "   Остались вопросы или предложения по работе чат-бота? Пиши нам!\n\n"
-        "Скорее выбирай, что мы будем делать👇",
-        [InteractiveMedia(
-            actions=[
-                InteractiveButton("У меня есть идея!💌", "У меня есть идея!💌"),
-                InteractiveButton("АИ-агенты?📍", "АИ-агенты?📍"),
-                InteractiveButton("Кто поможет?💬", "Кто поможет?💬"),
-                InteractiveButton("Поддержка📝", "Поддержка📝"),
-            ]
-        )]
-    )
-
-
 def start_handler(message: UpdateMessage) -> None:
-    bot.messaging.send_message(message.peer, "👋 Привет! Я Агентолог — бот, который помогает оценить идеи для AI-агентов!")
+    bot.messaging.send_message(message.peer, """
+👋 Привет, @user_name!
+    Меня зовут *Агентолог*, я помогу тебе с идеями для AI-агентов.
 
+    Вот что я могу сделать:
+    1. *У меня есть идея!*💡
+       Я помогу тебе узнать, твоя идея уникальна!
+    2. *АИ-агенты?*📍
+      АИ-агенты разрабатываются каждый день, здесь мы собрали самый свежий список агентов!
+    3. *Кто поможет?*💬
+       Агентов очень много и не всегда можно найти, кто их разрабатывает. Давай подскажем, кто эти люди!
+    4. *Поддержка📝*
+      Остались вопросы или предложения по работе чат-бота? Пиши нам!
+    Скорее выбирай, что мы будем делать, просто напиши текстом!
+""")
+
+def idea_handler(message: UpdateMessage) -> None:
+    peer = message.peer
+    bot.messaging.send_message(peer, "💬 Опиши свою идею свободно, я проверю её уникальность:")
+    user_states[message.message.sender_uid] = {"mode": "freeform"}
+
+def agent_handler(message: UpdateMessage) -> None:
+    bot.messaging.send_message(message.peer, "📍 Отправялю тебе список самых свежих агентов:")
+
+def help_handler(message: UpdateMessage) -> None:
+    bot.messaging.send_message(message.peer, """
+📝 Поддержка:
+📬 Пишите нам: @sigma.sbrf.ru@22754707
+📞 Пишите нам: 
+📧 Пишите нам: sigma.sbrf.ru@22754707
+""")
+
+def group_handler(message: UpdateMessage) -> None:
+    bot.messaging.send_message(message.peer, "Давай поищем, кто это!")
 
 def main():
     global bot
@@ -138,16 +151,14 @@ def main():
 
     bot.messaging.command_handler([
         CommandHandler(start_handler, "start", description="Поздороваться"),
+        CommandHandler(idea_handler, "idea", description="Идея!"),
+        CommandHandler(agent_handler, "ai_agent", description="Аи-агенты!"),
+        CommandHandler(group_handler, "group", description="Группа разработки"),
+        CommandHandler(help_handler, "help", description="Помощь"),
     ])
 
-    bot.messaging.message_handler([
-        MessageHandler(text_handler, MessageContentType.TEXT_MESSAGE),
-    ])
-
-    print("✅ Бот успешно запущен и готов к работе.")
-    while True:
-        pass
-
+    bot.messaging.message_handler([MessageHandler(text_handler, MessageContentType.TEXT_MESSAGE)])
+    bot.updates.on_updates(do_read_message=True, do_register_commands=True)
 
 if __name__ == "__main__":
     main()
